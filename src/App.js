85dadc74bc28588eb10207a0155810bce3fce41e
jelmer-agricola <jelmer.agricola@gmail.com>
2023-01-27 14:14:20 +0100

    opzet

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<h1>home</h1>
+      // <>
+      //   <Routes>
+      //
+      //     <Route path="/" element={<Home/>} />
+      //     {/*dynamische routing voor verschillende dingen .5*/}
+      //     <Route path="/movie/:id"/>
+      //     {/*<Route exact path="/login" element=<Login toggleAuthenticated={toggleAuth}/>}/>*/}
+      //     <Route exact path="/login" element=<Login />/>
+      //
+      //   </Routes>
+      // </>
   );
 }
 
