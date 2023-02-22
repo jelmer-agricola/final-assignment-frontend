d67013d0d22f97c90f6de2441e64408159b3b65d
jelmer-agricola <jelmer.agricola@gmail.com>
2023-02-22 17:28:53 +0100

    add test

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
     return (<section className="outer-content-container">
         <div className="inner-content-container">
-<p>Welcome to ... movies and series </p>
+            <p>Welcome to ... movies and series test</p>
             <h2> Don't know what to watch and too lazy to use the searchbar <Link to="/moods">click here</Link></h2>
 
             <div className="trending-container">
