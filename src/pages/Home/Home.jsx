ba52ce91497c90931395376a8108dd4735ec533a
jelmer-agricola <jelmer.agricola@gmail.com>
2023-02-24 14:19:57 +0100

    add button shadow and link shadow

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,18 +6,23 @@ import {Link} from "react-router-dom";
 const Home = () => {
 
     return (
-        <section className="outer-content-container">
-            <div className="inner-content-container">
-                <p>Welcome to your favorite website for movies and series login to see your watchlist and favoriteslist
-                    or Register to start creating one. </p>
-                <h2> Don't know what to watch by giving us your mood we can select the best movies for each mood for You! <Link to="/moods">click here</Link></h2>
-                <div className="trending-container">
-                    <TrendingMovies/>
-                    <TrendingSeries/>
+        <main>
+            <section className="outer-content-container">
+                <div className="inner-content-container">
+                    <p>Welcome to your favorite website for movies and series login to see your watchlist and
+                        favoriteslist
+                        or Register to start creating one. </p>
+                    <h2> Don't know what to watch by giving us your mood we can select the best movies for each mood for
+                        You! <Link to="/moods">click here</Link></h2>
+                    <div className="trending-container">
+                        <TrendingMovies/>
+                        <TrendingSeries/>
+                    </div>
                 </div>
-            </div>
 
-        </section>);
+            </section>
+        </main>
+    );
 };
 
 export default Home;
